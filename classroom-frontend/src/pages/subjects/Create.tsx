82e030c21a01309ebe18081a19a18851b59177a0
Samvel Toroyan <storoyan@fpkinvest.it>
2026-01-28 15:23:18 +0300

    Initial commit

---
+++ classroom-frontend/src/pages/subjects/Create.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SubjectsCreate = () => {
+  return <div>SubjectsCreate</div>;
+};
+
+export default SubjectsCreate;
---
+++ classroom-frontend/src/pages/subjects/List.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SubjectsList = () => {
+  return <div>List</div>;
+};
+
+export default SubjectsList;
