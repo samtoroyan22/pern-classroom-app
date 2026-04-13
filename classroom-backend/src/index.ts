a290e1f46c0f2d673ed2945f00b0f09bd877e67a
Samvel Toroyan <storoyan@fpkinvest.it>
2026-04-13 10:46:57 +0300

    implement GET all subjects route

--- classroom-backend/src/index.ts
+++ classroom-backend/src/index.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import subjectRouter from "./routes/subjects.js";
+import cors from "cors";
 
 const app = express();
 const PORT = 8000;
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
+
+app.use("/api/subjects", subjectRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, Welcome to the classroom API");
