da2fd991a6d3e1592018c808486003b6b2f04110
Samvel Toroyan <storoyan@fpkinvest.it>
2026-04-21 15:58:17 +0300

    implement classes component, upload widget

--- classroom-frontend/src/App.tsx
+++ classroom-frontend/src/App.tsx
@@ -13,12 +13,16 @@ import { useNotificationProvider } from "./components/refine-ui/notification/use
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
 
-import { BookOpen, Home } from "lucide-react";
+import { BookOpen, GraduationCap, Home } from "lucide-react";
 import { Layout } from "./components/refine-ui/layout/layout";
 
 import SubjectsCreate from "./pages/subjects/Create";
-import Dashboard from "./pages/Dashboard";
 import SubjectsList from "./pages/subjects/List";
+
+import ClassesCreate from "./pages/classes/create";
+import ClassesList from "./pages/classes/list";
+
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -47,6 +51,12 @@ function App() {
                   create: "/subjects/create",
                   meta: { label: "Subjects", icon: <BookOpen /> },
                 },
+                {
+                  name: "classes",
+                  list: "/classes",
+                  create: "/classes/create",
+                  meta: { label: "Classes", icon: <GraduationCap /> },
+                },
               ]}
             >
               <Routes>
@@ -61,6 +71,10 @@ function App() {
                   <Route path="/subjects">
                     <Route index element={<SubjectsList />} />
                     <Route path="create" element={<SubjectsCreate />} />
+                  </Route>
+                  <Route path="/classes">
+                    <Route index element={<ClassesList />} />
+                    <Route path="create" element={<ClassesCreate />} />
                   </Route>
                 </Route>
               </Routes>
